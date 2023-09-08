52f74c5a4515781c3a99b82554feffbb8411289f
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2023-09-08 09:04:57 +0600

    [TRY-1] deploy on vercel

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,21 @@
-import { Server } from 'http';
-import app from './app';
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
 import config from './config';
-import { errorlogger, logger } from './shared/logger';
 
+const prisma = new PrismaClient();
+const app = express();
 
-async function bootstrap() {
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log('Database is connected successfully....');
 
-  const server: Server = app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`);
-  });
-
-  const exitHandler = () => {
-
-    if (server) {
-      server.close(() => {
-        logger.info('Server closed');
-      });
-    }
-    process.exit(1);
-  };
-
-  const unexpectedErrorHandler = (error: unknown) => {
-    errorlogger.error(error);
-    exitHandler();
-  };
-
-  process.on('uncaughtException', unexpectedErrorHandler);
-  process.on('unhandledRejection', unexpectedErrorHandler);
-
-  process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
-    if (server) {
-      server.close();
-    }
-  });
+    app.listen(config.port, () => {
+      console.log(`App is listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.error('Failed to connect to the database....', error);
+  }
 }
 
-bootstrap();
+main();
