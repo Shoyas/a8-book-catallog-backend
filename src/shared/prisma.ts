58ad3453d1234ea9b6924f295603ed19e44dfa63
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2023-09-15 20:04:17 +0600

    [TRY-SOLUTION] reconnecting database after deploy

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import config from '../config';
 
 const prisma = new PrismaClient({
   errorFormat: 'minimal',
+  datasources: { db: { url: config.database_url } },
 });
 
 export default prisma;
+
+// const prisma = new PrismaClient({ datasources: {  db: { url: "mysql://yourdburlstringhere" } } });
